9462e5067db405898d18256b2eb4adbb11b445c4
diegoperea20 <diegoperea20@gmail.com>
2024-06-29 21:57:30 -0500

    v1

--- README.md
+++ README.md
@@ -1,8 +1,54 @@
+# Nextjs Minimum Temperature Predictions since 1991 in Melbourne
+
+Nextjs Minimum Temperature Predictions since 1991 in Melbourne with dataset [Daily Minimum Temperatures in Melbourne](https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv) 
+
+Model used from [Modelos-de-Pronostico-de-Temperatura-Minima-en-Melbourne](https://github.com/diegoperea20/Modelos-de-Pronostico-de-Temperatura-Minima-en-Melbourne/tree/main/modeloMLP) for his size 
+
+In "End date" , Select the date you want to predict the temperature and the red graph will predict it
+<p align="center">
+  <img src="README-images/home.PNG" alt="Step1">
+</p>
+
+<p align="center">
+  <img src="README-images/newplot.png" alt="Step2">
+</p>
+
+Prediction for specific date
+First Select the date you want to predict  With button "Preddic temperature" clicked ans show message 
+"Predicted temperature for 1998-01-01: 14.32"
+
+
+----
+Convert h5 to Tensorflowjs in ipynb
+```bash
+!pip install tensorflowjs
+```
+```bash
+!tensorflowjs_converter --input_format keras /root/to/your/model.h5 /root/to/your/folder/save
+```
+-----
+
+-----
+
+Install nextjs libraries tensorflow
+```bash
+npm install @tensorflow/tfjs @tensorflow/tfjs-backend-cpu @tensorflow/tfjs-backend-webgl
+```
+-----
+
+Fronted Nextjs Options for do it:
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
+Nodejs version v20.10.0 and Next.js version v14.2.3 
 
-First, run the development server:
+First
+```bash
+npm install
+```
+run the development server:
 
 ```bash
 npm run dev
@@ -16,21 +62,13 @@ bun dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+## Resolve : Error Nextjs Parsing error: Cannot find module 'next/babel'
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Put this code in .eslintrc.json 
+```bash
+{
+  "extends": ["next/babel","next/core-web-vitals"]
+}
+```
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Created by [Diego Ivan Perea Montealegre](https://github.com/diegoperea20)
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,226 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+import * as dfd from "danfojs";
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
 
-export default function Home() {
+const ModelComponent = () => {
+  const [model, setModel] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+  const [datesToPredict, setDatesToPredict] = useState([]);
+  const [realTemps, setRealTemps] = useState([]);
+  const [realDates, setRealDates] = useState([]);
+  const [scalerFeatures, setScalerFeatures] = useState(null);
+  const [scalerTemp, setScalerTemp] = useState(null);
+  const [endDate, setEndDate] = useState('1992-12-31');
+  const [predictionDate, setPredictionDate] = useState('1998-01-01');
+  const [isLoading, setIsLoading] = useState(true);
+  const [isEndDateLoading, setIsEndDateLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadModel() {
+      setIsLoading(true);
+      const modelUrl = `${window.location.origin}/model/model.json`;
+      const loadedModel = await tf.loadLayersModel(modelUrl);
+      setModel(loadedModel);
+      console.log('Modelo cargado');
+      setIsLoading(false);
+    }
+    loadModel();
+  }, []);
+
+  const fetchData = async () => {
+    setIsEndDateLoading(true);
+    const url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv';
+    const df = await dfd.readCSV(url);
+
+    const scalerFeatures = new dfd.MinMaxScaler();
+    const scalerTemp = new dfd.MinMaxScaler();
+
+    const day = df['Date'].map(date => new Date(date).getDate());
+    const month = df['Date'].map(date => new Date(date).getMonth() + 1);
+    const year = df['Date'].map(date => new Date(date).getFullYear());
+    const temp = df['Temp'];
+
+    const features = await dfd.concat({ dfList: [day, month, year], axis: 1 });
+    const normalizedFeatures = scalerFeatures.fitTransform(features);
+    const normalizedTemp = scalerTemp.fitTransform(temp);
+
+    setScalerFeatures(scalerFeatures);
+    setScalerTemp(scalerTemp);
+
+    const realDates = df['Date'].values;
+    const realTemps = temp.values;
+    setRealDates(realDates);
+    setRealTemps(realTemps);
+
+    const startDate = new Date('1991-01-01');
+    const endDateObj = new Date(endDate);
+    const datesToPredict = [];
+    for (let date = startDate; date <= endDateObj; date.setDate(date.getDate() + 1)) {
+      datesToPredict.push(date.toISOString().split('T')[0]);
+    }
+    setDatesToPredict(datesToPredict);
+
+    if (model) {
+      const predictions = [];
+      for (let dateStr of datesToPredict) {
+        const dateObj = new Date(dateStr);
+        const day = dateObj.getDate();
+        const month = dateObj.getMonth() + 1;
+        const year = dateObj.getFullYear();
+        const inputTensor = tf.tensor2d([[day, month, year]]);
+        const scaledInput = scalerFeatures.transform(inputTensor);
+        const predictionTensor = model.predict(scaledInput);
+        const prediction = scalerTemp.inverseTransform(predictionTensor).dataSync()[0];
+        predictions.push(prediction);
+      }
+      setPredictions(predictions);
+    }
+    setIsEndDateLoading(false);
+  };
+
+  useEffect(() => {
+    if (model) {
+      fetchData();
+    }
+  }, [model, endDate]);
+
+  const predictTemperature = async (dateStr) => {
+    if (!model || !scalerFeatures || !scalerTemp) {
+      console.error('Modelo o escaladores no cargados');
+      return null;
+    }
+
+    const dateObj = new Date(dateStr);
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1;
+    const year = dateObj.getFullYear();
+
+    const inputTensor = tf.tensor2d([[day, month, year]]);
+    const scaledInput = scalerFeatures.transform(inputTensor);
+    const predictionTensor = model.predict(scaledInput);
+    const prediction = scalerTemp.inverseTransform(predictionTensor).dataSync()[0];
+
+    return prediction;
+  };
+
+  if (isLoading) {
+    return <div className="div-flex"><h1>Loading model...</h1><div className="loader">
+    <div className="bar1"></div>
+    <div className="bar2"></div>
+    <div className="bar3"></div>
+    <div className="bar4"></div>
+    <div className="bar5"></div>
+    <div className="bar6"></div>
+    <div className="bar7"></div>
+    <div className="bar8"></div>
+    <div className="bar9"></div>
+    <div className="bar10"></div>
+    <div className="bar11"></div>
+    <div className="bar12"></div>
+</div></div>;
+  }
+
+  const plotLayout = {
+    title: 'Predictions and Real Data',
+    xaxis: { title: 'Date' },
+    yaxis: { title: 'Temperature' },
+    paper_bgcolor: '#333', // Color de fondo del papel (área fuera del gráfico)
+    plot_bgcolor: '#333',  // Color de fondo del área de trazado
+    font: { color: 'white' }, // Color de fuente general
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <h1>Minimum Temperature Predictions since 1991 in Melbourne</h1>
+      <div>
+        <label htmlFor="end-date">End date:</label>
+        <input
+          type="date"
+          id="end-date"
+          value={endDate}
+          onChange={(e) => {
+            setEndDate(e.target.value);
+            setIsEndDateLoading(true);
+          }}
         />
+        {isEndDateLoading && <span> <div className="loader">
+    <div className="bar1"></div>
+    <div className="bar2"></div>
+    <div className="bar3"></div>
+    <div className="bar4"></div>
+    <div className="bar5"></div>
+    <div className="bar6"></div>
+    <div className="bar7"></div>
+    <div className="bar8"></div>
+    <div className="bar9"></div>
+    <div className="bar10"></div>
+    <div className="bar11"></div>
+    <div className="bar12"></div>
+</div>Loading new data...</span>}
+      </div>
+      {isEndDateLoading ? (
+        <div>Loading predictions...</div>
+      ) : (
+        <Plot
+          data={[
+            {
+              x: datesToPredict,
+              y: predictions,
+              type: 'scatter',
+              mode: 'lines',
+              name: 'Predictions',
+              line: { color: 'red' },
+            },
+            {
+              x: realDates,
+              y: realTemps,
+              type: 'scatter',
+              mode: 'lines',
+              name: 'Real Data',
+              line: { color: 'green' },
+            },
+          ]}
+          layout={plotLayout}
+          useResizeHandler={true}
+          style={{ width: "100%", height: "500px" }}
+          config={{ responsive: true, displayModeBar: true, }}
+        />
+      )}
+      <div>
+        <h2>Prediction for specific date</h2>
+        <input
+          type="date"
+          value={predictionDate}
+          onChange={(e) => setPredictionDate(e.target.value)}
+        />
+        <button onClick={async () => {
+          setIsLoading(true);
+          const temp = await predictTemperature(predictionDate);
+          setIsLoading(false);
+          if (temp !== null) {
+            alert(`Predicted temperature for ${predictionDate}: ${temp.toFixed(2)}`);
+          } else {
+            alert('The prediction could not be made. Make sure the model is loaded.');
+          }
+        }}>
+          Predict temperature
+        </button>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="project-github">
+        <p>This project is in</p>
+        <Link href="https://github.com/diegoperea20">
+          <img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/>
+        </Link>
       </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default ModelComponent;
